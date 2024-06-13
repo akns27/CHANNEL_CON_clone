68280e5d8707ec254c8bbcc00304132f77d2d01d
KangwonPark <nambaggangwon@gmail.com>
2024-06-13 10:52:48 +0900

    ADD :: 로고 추가

--- src/pages/HeroPage.jsx
+++ src/pages/HeroPage.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import "../style.css";
+
+
 
 function HeroPage() {
   return (
     <div>
-      <img src='https://raw.githubusercontent.com/akns27/CHANNEL_CON_clone/master/src/assets/HeroPage.png'></img>
+      <img className='hero-banner' src='https://raw.githubusercontent.com/akns27/CHANNEL_CON_clone/dcdaecdebe7d4e21ba5da642073dbe7616829eb1/src/assets/HeroBanner.svg'></img>
+      <div><img src=''></img></div>
+      <div>LET'S TALK FUTURE</div>
+      <div>2024.04.03 그랜드 인터컨티넨탈 서울 파르나스</div>
+      <div>응모가 마감되었습니다</div>
     </div>
   );
 }
